f9f34c69ad38893183c9b38488f26dc11317b872
Илья Иванов <ivilya07@gmail.com>
2026-06-14 11:22:37 +0300

    Сделали практические задания для функций

--- functions/func.py
+++ functions/func.py
@@ -1,17 +1,17 @@
 # Вызов функции
-def greet(name):
-    print(f"Hello my dear {name}")
+# def greet(name):
+#     print(f"Hello my dear {name}")
 
-greet("John")
+# greet("John")
 
 # Автоматическая упаковка в кортеж
-def get_user_info():
-    return "ivan", 25, "Moscow"
+# def get_user_info():
+#     return "ivan", 25, "Moscow"
 
-name, age, city = get_user_info() # Возовращает кортеж данных
+# name, age, city = get_user_info() # Возовращает кортеж данных
 
-print(name)
-print(get_user_info())
+# print(name)
+# print(get_user_info())
 
 # Параметры и аргументы
 # Параметры — это переменные в скобках при объявлении функции.
@@ -26,22 +26,22 @@ print(get_user_info())
 # параметры со значением по умолчанию должны идти после обязательных параметров
 
 
-def power(base, exp=2):
-    return base ** exp
+# def power(base, exp=2):
+#     return base ** exp
 
-print(power(2))
-print(power(3, 3))
+# print(power(2))
+# print(power(3, 3))
 
 # Произвольное количество агрументов *args и **kwargs
 # *args собирает все переданные позиционные аргументы в кортеж
 # **kwargs собирает все переданные именованные аргументы в словарь
 
-def print_info(*args, **kwargs):
-    print("Позиционные: ", args)
-    print("Именнованые: ", kwargs)
+# def print_info(*args, **kwargs):
+#     print("Позиционные: ", args)
+#     print("Именнованые: ", kwargs)
 
-print_info(1, 2, 3, name = "Ann", age = 30, city = "NY")
-print_info(7,8,9, "glev", "sup")
+# print_info(1, 2, 3, name = "Ann", age = 30, city = "NY")
+# print_info(7,8,9, "glev", "sup")
 
 
 # Область видимости
@@ -58,101 +58,210 @@ print_info(7,8,9, "glev", "sup")
 #     global: позволяет изменять глобальную переменную внутри функции.
 #     nonlocal: позволяет изменять переменную из внешней (но не глобальной) функции.
 
-x = "global"
+# x = "global"
 
-def outer():
-    x = "Enclosing"
-    def inner():
+# def outer():
+#     x = "Enclosing"
+#     def inner():
         # nonlocal вынуждает x быть равным "local" в outer
         # он говорит x что не нужно создавать новую переменную, а следует привязаться к ближайшей x
         # из внешней функции, тоесть outer. Теперь оба x ссылаются на один и тот же объект памяти
         # доказно с помощью функции id()
-        nonlocal x
-        x = "local"
-        print("in inner ", x)
-        print(id(x))
-    inner()
-    print("in outer ", x)
-    print(id(x))
+#         nonlocal x
+#         x = "local"
+#         print("in inner ", x)
+#         print(id(x))
+#     inner()
+#     print("in outer ", x)
+#     print(id(x))
 
-outer()
+# outer()
 
 
 # global 
 
 # без global
 
-count = 0
+# count = 0
 
-def increment():
+# def increment():
 #     # Создается новая локальная переменная count
-    count = 1
-    print(count)
+#     count = 1
+#     print(count)
 
-increment()
-print(count) # Выводиться глобальная переменная
+# increment()
+# print(count) # Выводиться глобальная переменная
 
 # С global
 
-count = 0
+# count = 0
 
-def increment():
-    global count
-    count +=1
-    print(count)
+# def increment():
+#     global count
+#     count +=1
+#     print(count)
 
-increment()
-print(count) # Изменилась глобальная переменная
+# increment()
+# print(count) # Изменилась глобальная переменная
 
 # Анонимные функции lambda
 # Использовать только тогда когда можно поместить в одну простую конструкцию
 
-square = lambda x: x ** 2
-print(square(5))
+# square = lambda x: x ** 2
+# print(square(5))
 
-pairs = [(1, "one"), (3, "three"), (2, "two")]
-pairs.sort(key=lambda pair: pair[0])
-print(pairs)
+# pairs = [(1, "one"), (3, "three"), (2, "two")]
+# pairs.sort(key=lambda pair: pair[0])
+# print(pairs)
 
 
 # Функции как объекты в других функциях
 
-def shout(text):
-    return text.upper()
+# def shout(text):
+#     return text.upper()
 
-def whisper(text):
-    return text.lower()
+# def whisper(text):
+#     return text.lower()
 
-def process_to_text(func, text):
-    return func(text)
+# def process_to_text(func, text):
+#     return func(text)
 
 #  Передаем функцию как один из аргументов
-print(process_to_text(whisper, text="FDSFDSF"))
+# print(process_to_text(whisper, text="FDSFDSF"))
 
 
 # Декарторы
 # Декоратор — это функция, которая принимает другую функцию и расширяет её поведение,
 #  не изменяя её код напрямую. Используется синтаксис @.
 
-def my_decarator(func):
-    def wrapper():
-        print("Действие до вызова функции")
-        func()
-        print("Действие после вызова функции")
+# def my_decarator(func):
+#     def wrapper():
+#         print("Действие до вызова функции")
+#         func()
+#         print("Действие после вызова функции")
+#     return wrapper
+
+
+# @my_decarator
+# def say_hello():
+#     print("Привет")
+
+
+# say_hello()
+
+# Type hints и Docstring указание типов для читаемости и проверки
+# def calculate_area(radius: float) -> float:
+#     import math
+#     result = math.pi * (radius ** 2)
+#     print(result)
+
+# calculate_area(4)
+
+# Практические задания от QWEN
+
+# class WrongOperation(Exception):
+#     pass
+
+# def caclulate(a: float, b: float, operation: str) -> float:
+#     operations = ["+", "-", "*", "/"]
+#     if operation not in operations:
+#         raise WrongOperation
+    
+#     if operation == "+":
+#         result = a + b
+#     elif operation == "-":
+#         result = a - b
+#     elif operation == "*":
+#         result = a * b
+#     elif operation == "/":
+#         if b == 0:
+#             raise ZeroDivisionError
+#         result = a / b
+#     return result
+
+# try:
+#     print(caclulate(a=4.5, b=5, operation="//"))
+# except WrongOperation:
+#     print("Неверная операция")
+
+# try:
+#     print(caclulate(a=5,b=0,operation="/"))
+# except ZeroDivisionError:
+#     print("Деление на ноль")
+
+# print(caclulate(a=5,b=7.2,operation="*"))
+
+
+
+# def is_palindrome(text: str) -> bool:
+#     cleaned_text = "".join(text.split()).lower() # удаляем пробелы и переводим в нижний регистр(join скливает слова в одну строку)
+#     return cleaned_text == cleaned_text[::-1]
+
+# print(is_palindrome("Шалаш"))
+
+# import statistics
+
+# def get_stats(*numbers):
+#     min_num = min(numbers)
+#     max_num = max(numbers)
+#     average = statistics.mean(numbers) #Находим среднее арифметическое
+#     anwers = {"minimum": min_num, "maximum": max_num, "average ": average}
+#     return anwers
+
+
+# print(get_stats(1, 3, 6, 8, 342, 7))
+
+
+# def filter_strings(**kwargs):
+#     new_dict = {}
+#     for key, value in kwargs.items():
+#         if type(value) is str:
+#            new_dict.update({f"{key}": value})
+#     return new_dict
+
+
+# print(filter_strings(num = 41141, name = "BLen", surname = "NLSF", index = 4923))
+
+# def make_multiplier(n):
+#     def make_pros(x):
+#         result = x * n
+#         return result
+#     return make_pros
+    
+
+# double = make_multiplier(2)
+
+# print(double(5))
+
+# def my_decarator(func):
+#     def wrapper():
+#         print("Действие до вызова функции")
+#         func()
+#         print("Действие после вызова функции")
+#     return wrapper
+
+
+# @my_decarator
+# def say_hello():
+#     print("Привет")
+
+import time
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        print("Начало работы функции")
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+        print("Конец выполнения функции")
+        execution_time = end - start
+        print(f"-> Время выполнения: {execution_time:.6f} сек.")
+        return result
     return wrapper
 
 
-@my_decarator
-def say_hello():
-    print("Привет")
+@timer
+def say_hello(name):
+    print(f"HEllo {name}")
 
-
-say_hello()
-
-# Type hints и Docstring указание типов для читаемости и проверки
-def calculate_area(radius: float) -> float:
-    import math
-    result = math.pi * (radius ** 2)
-    print(result)
-
-calculate_area(4)
+print(say_hello("Дмитрий"))

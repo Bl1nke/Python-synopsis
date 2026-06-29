925f530d7ed4b8b2cb6e930f616103d1c971b534
Илья Иванов <ivilya07@gmail.com>
2026-06-29 11:47:56 +0300

    Добавили защиту, если файл пустой

--- json_learning/third_task.py
+++ json_learning/third_task.py
@@ -1,15 +1,16 @@
 import json
-
-with open("json_learning/users.json", "r", encoding="utf-8") as file:
-    # json.load автоматически определяет структуру json файла
-    data = json.load(file)
-    # enumerate позволяет нам создать index для автоматического подсчета количества(счетчик элементов + автоматическое увелечение счетчика +=1)
-    for index, item in enumerate(data):
-        user_id = item.get("id")
-        user_login = item.get("login")
-        user_status = item.get("is_active")
-
-        print(f"Пользователь {user_login} с id {user_id} со статусом {user_status}")
+try:
+    with open("json_learning/users.json", "r", encoding="utf-8") as file:
+        # json.load автоматически определяет структуру json файла
+        data = json.load(file)
+        # enumerate позволяет нам создать index для автоматического подсчета количества(счетчик элементов + автоматическое увелечение счетчика +=1)
+        for index, item in enumerate(data):
+            user_id = item.get("id")
+            user_login = item.get("login")
+            user_status = item.get("is_active")
+            print(f"Пользователь {user_login} с id {user_id} со статусом {user_status}")
+except (FileNotFoundError, json.JSONDecodeError):
+    data = []
                 
 
 Bill_data = {"id": 4324, "login": "Билл", "is_active": True}
@@ -21,3 +22,4 @@ with open("json_learning/users.json", "w", encoding="utf-8") as file:
     json.dump(data, file, indent=2, ensure_ascii=False)
     print("Пользователь добавлен")
 
+# Сделать защиту если файл пустой и записывать туда только новый
